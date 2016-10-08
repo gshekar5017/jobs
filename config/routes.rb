5b15f94ac511688615eb513bed007a100b87ac93
gshekar5017 <shekargudepu@gmail.com>
2016-10-08 04:02:38 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :professors
-  get 'home/index'
+
 
   devise_for :students
   # The priority is based upon order of creation: first created -> highest priority.
